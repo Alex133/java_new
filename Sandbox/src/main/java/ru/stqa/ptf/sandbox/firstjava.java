71ec42946410885b2ec6de42549ed5ec495093ed
Alex133 <katsnelson82@gmail.com>
2021-01-11 23:27:30 +0200

    added functions

--- Sandbox/src/main/java/ru/stqa/ptf/sandbox/firstjava.java
+++ Sandbox/src/main/java/ru/stqa/ptf/sandbox/firstjava.java
@@ -1,7 +1,28 @@
 package ru.stqa.ptf.sandbox;
 
 public class firstjava {
-	public static void main (String []args){
-		System.out.println("Hello, world!");
+	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("A");
+
+		double l =5 ;
+		double a =4 ;
+		double  b =6;
+		System.out.println("area of " + a + " and " + b + " = " + area(a,b));
+		System.out.println("square area " + l + " = " + area(l));
 	}
-}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+		public static double area(double len){
+			return len * len;
+
+		}
+
+		public static double area (double a, double b){
+		return a *b;
+		}
+	}
+
