3828ad53641448609ccdb380584ef6ec36542448
Alex133 <katsnelson82@gmail.com>
2021-01-18 22:21:58 +0200

    a

--- Sandbox/src/main/java/ru/stqa/ptf/sandbox/firstjava.java
+++ Sandbox/src/main/java/ru/stqa/ptf/sandbox/firstjava.java
@@ -6,23 +6,27 @@ public class firstjava {
 		hello("user");
 		hello("A");
 
-		double l =5 ;
-		double a =4 ;
-		double  b =6;
-		System.out.println("area of " + a + " and " + b + " = " + area(a,b));
-		System.out.println("square area " + l + " = " + area(l));
+		Square s = new Square (5);
+		///s.l = 5;
+		System.out.println("Ploshad kvadrata so storonoi " + s.l +" = " + s.area ());
+
+		Rectangle  r = new Rectangle (4, 6);
+
+		System.out.println("Ploshad rectangle so storonoi " + r.a +" = " + r.area ());
+
+
+		//double l =5 ;
+		//double a =4 ;
+		//double  b =6;
+		//System.out.println("area of " + a + " and " + b + " = " + area (a,b));
+		//System.out.println("square area " + l + " = " + area(l));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
-		public static double area(double len){
-			return len * len;
 
-		}
 
-		public static double area (double a, double b){
-		return a *b;
-		}
+
 	}
 
