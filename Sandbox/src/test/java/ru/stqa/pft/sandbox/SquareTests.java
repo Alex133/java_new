31c9d4fdd92ea46cb08c13d04e866d6c55be3362
Alex133 <katsnelson82@gmail.com>
2021-01-21 21:34:22 +0200

    a

--- Sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -10,6 +10,7 @@ public class SquareTests {
 
     public void testArea() {
         Square s = new Square(5);
+        
        // assert s.area() == 25 ;
         Assert.assertEquals(s.area(),25.0);
     }
