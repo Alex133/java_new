4007c2ae3f69b9767bdd7bfb3fdd9a36980de899
Alex133 <katsnelson82@gmail.com>
2021-01-23 14:19:54 +0200

    1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -26,23 +26,23 @@ public class GroupCreationTests {
   @Test
   public void GroupCreationTests() throws Exception {
     driver.get("http://localhost/addressbook/");
-    driver.findElement(By.linkText("Logout")).click();
-    driver.findElement(By.name("user")).click();
+    //driver.findElement(By.linkText("Login")).click();
+    //driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).click();
     // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=user | ]]
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.id("LoginForm")).click();
+    //driver.findElement(By.id("LoginForm")).click();
     driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
     // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=pass | ]]
     driver.findElement(By.xpath("//input[@value='Login']")).click();
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test5");
+    driver.findElement(By.name("group_name")).sendKeys("test15");
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
     driver.findElement(By.name("group_header")).sendKeys("test6");
@@ -53,39 +53,8 @@ public class GroupCreationTests {
     driver.findElement(By.linkText("group page")).click();
     driver.findElement(By.xpath("(//input[@name='selected[]'])[5]")).click();
     driver.findElement(By.name("user")).click();
-    driver.close();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.id("LoginForm")).submit();
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("user")).click();
-    driver.close();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.id("content")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test9");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test");
-    driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
-    driver.findElement(By.xpath("//div[@id='content']/form/span")).click();
-    driver.findElement(By.name("selected[]")).click();
-    driver.findElement(By.xpath("(//input[@name='selected[]'])[2]")).click();
-    driver.findElement(By.xpath("(//input[@name='selected[]'])[3]")).click();
-    driver.findElement(By.linkText("groups")).click();
+
   }
 
   @AfterClass(alwaysRun = true)
